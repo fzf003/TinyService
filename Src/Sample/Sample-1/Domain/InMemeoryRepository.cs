454b2af3656d7738bf47021124c40e054ba9753d
fzf003 <280086260@qq.com>
2020-07-14 15:57:24 +0800

    更新文件

--- Src/Sample/Sample-1/Domain/InMemeoryRepository.cs
+++ Src/Sample/Sample-1/Domain/InMemeoryRepository.cs
@@ -15,11 +15,11 @@ namespace Sample_1.Domain
 
     public class InMemeoryRepository : IInMemeoryRepository
     {
-        private List<Product> _employeesList;
+        private List<Product> _productList;
 
         public InMemeoryRepository()
         {
-            _employeesList = new List<Product>()
+            _productList = new List<Product>()
             {
 
             };
@@ -27,28 +27,28 @@ namespace Sample_1.Domain
 
         public IEnumerable<Product> GetAll()
         {
-            return _employeesList.ToList();
+            return _productList.ToList();
         }
 
         public Product Get(int id)
         {
-            return _employeesList.FirstOrDefault(e => e.Id == id);
+            return _productList.FirstOrDefault(e => e.Id == id);
         }
 
 
         public void Save(Product item)
         {
-            // Check and if exists delete Employee with given id
+            
             Delete(item.Id);
-            _employeesList.Add(item);
+            _productList.Add(item);
         }
 
         public void Delete(int id)
         {
-            Product emp = _employeesList.FirstOrDefault(e => e.Id == id);
+            Product emp = _productList.FirstOrDefault(e => e.Id == id);
             if (emp != null)
             {
-                _employeesList.Remove(emp);
+                _productList.Remove(emp);
             }
         }
 
